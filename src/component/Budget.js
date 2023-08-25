27a24bbaa4e64ecab43c037d64da828308c5c6f4
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-08-25 22:57:34 +0200

    budget edit dispatch function updated

--- src/component/Budget.js
+++ src/component/Budget.js
@@ -1,10 +1,32 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
+import ViewBudget from './ViewBudget';
+import EditBudget from './EditBudget';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const {budget} = useContext(AppContext);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const {budget, dispatch} = useContext(AppContext);
+
+    const handleEditClick = () => {
+        setIsEditing(true);
+    };
+
+    const handleSaveClick = (value) => {
+        dispatch({
+            type : 'SET_BUDGET',
+            payload : value,
+        });
+        setIsEditing(false)
+    }
+
     return (
-        <div className='alert alert-secondary'>
+        <div className='alert alert-secondary p-3 d-flex align-items-center justify-content-between'>
+            {isEditing ? (
+                <EditBudget handleSaveClick={handleSaveClick} budget={budget} />
+            ) : (
+                <ViewBudget handleEditClick={handleEditClick} budget={budget} />
+            )}
             <span>Budget : £{budget}</span>
         </div>
     )
